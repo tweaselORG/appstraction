4f5c90ae60e28c017febc4e0398fd0f92fcfec42
Benjamin Altpeter <hi@bn.al>
2023-02-03 14:54:41 +0100

    Support for physical Android devices
    There aren't too many differences compared to emulators, most notably:
    
    * The device preparation steps are obviously different.
    * Resetting isn't possible.
    * For setting the clipboard, we can't rely on the device using the same
      launcher as the emulators (that was a bad assumption even then), so
      we need to find some other app we can inject Frida into.

---
+++ examples/android-device.ts
@@ -0,0 +1,43 @@
+/* eslint-disable no-console */
+import { homedir } from 'os';
+import { join } from 'path';
+import { pause, platformApi } from '../src/index';
+
+// You can pass the following command line arguments:
+// `npx tsm examples/android.ts <app ID> <app path>`
+
+(async () => {
+    const android = platformApi({
+        platform: 'android',
+        runTarget: 'device',
+        capabilities: ['frida', 'certificate-pinning-bypass'],
+        targetOptions: {
+            fridaPsPath: join(homedir(), '.local/bin/frida-ps'),
+            objectionPath: join(homedir(), '.local/bin/objection'),
+        },
+    });
+
+    const appId = process.argv[2] || 'de.hafas.android.db';
+    const appPath = process.argv[3] || '/path/to/app-files';
+
+    await android.ensureDevice();
+
+    await android.setClipboard('I copied this.');
+
+    const version = await android.getAppVersion(`${appPath}/${appId}/${appId}.apk`);
+    console.log('App version:', version);
+
+    await android.installApp(`${appPath}/${appId}/*.apk`);
+    await android.setAppPermissions(appId);
+    await android.startApp(appId);
+
+    // Give the app some time to start.
+    await pause(5000);
+
+    const prefs = await android.getPrefs(appId);
+    console.log(prefs);
+
+    await android.clearStuckModals();
+    await android.uninstallApp(appId);
+})();
+/* eslint-enable no-console */
