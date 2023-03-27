9157f594a1aed312b2425be7aa87cee511ddf48c
Lorenz Sieben <me@lorenz-sieben.com>
2023-03-27 12:39:05 +0200

    Wait for devices to complete their boot (important for emulators)

--- docs/README.md
+++ docs/README.md
@@ -40,7 +40,7 @@ An ID of a known permission on Android.
 
 #### Defined in
 
-[android.ts:714](https://github.com/tweaselORG/platform-apis/blob/main/src/android.ts#L714)
+[android.ts:718](https://github.com/tweaselORG/platform-apis/blob/main/src/android.ts#L718)
 
 ___
 
@@ -274,7 +274,7 @@ The IDs of known permissions on Android.
 
 #### Defined in
 
-[android.ts:583](https://github.com/tweaselORG/platform-apis/blob/main/src/android.ts#L583)
+[android.ts:587](https://github.com/tweaselORG/platform-apis/blob/main/src/android.ts#L587)
 
 ___
 
--- src/android.ts
+++ src/android.ts
@@ -46,10 +46,14 @@ export const androidApi = <RunTarget extends SupportedRunTarget<'android'>>(
         objectionProcesses: [],
 
         awaitAdb: async () => {
-            const adbIsStarted = await retryCondition(
-                async () => (await execa('adb', ['get-state'], { reject: false })).exitCode === 0,
-                100
-            );
+            const adbIsStarted = await retryCondition(async () => {
+                const { exitCode, stdout } = await execa(
+                    'adb',
+                    ['wait-for-device', 'shell', 'getprop', 'dev.bootcomplete'],
+                    { reject: false }
+                );
+                return exitCode === 0 && stdout.includes('1');
+            }, 100);
             if (!adbIsStarted) throw new Error('Failed to connect via adb.');
         },
         async ensureFrida() {
